388c0ffb8b185e091533e68e8347b69d84d2466f
aljko <aljkoroz@gmail.com>
2021-08-07 14:13:56 +0200

    commit préBomb

--- public/index.php
+++ public/index.php
@@ -1,4 +1,2 @@
 <?php
     require '../src/routing.php';
-
-    echo '<pre>'; var_dump($_SERVER); echo '</pre>';
--- src/routing.php
+++ src/routing.php
@@ -1,19 +1,18 @@
 <?php
 
 $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: Home);
-$controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
+$controller = 'App\\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
 $method = $routeParts[1] ?? '';
 $vars = array_slice($routeParts, 2);
 
-echo '<pre>'; var_dump($routeParts); echo '</pre>';
-echo '------------------------------------------';
-echo '<p>$controller : '.$controller.'</p>';
-echo '<p>$method : '.$method.'</p>';
-echo '<p>$vars : '.$vars.'</p>';
-
+if (class_exists('HomeController')){
+    echo "exist";
+}else{
+    echo "no exist";
+};
 
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
-    echo call_user_func_array([new $controller(), $method], $vars);
+    echo (new $controller())->$method(...$vars);
 } else {
     header("HTTP/1.0 404 Not Found");
     echo '404 - Page not found';
